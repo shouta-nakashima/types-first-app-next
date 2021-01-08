1ea11bf5f649a26e5ebd6135acdfc76de52a82ee
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2021-01-08 11:48:28 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react'
+//import Head from "next/head";
+//import styles from "../../styles/Home.module.css";
+
+const Home: FC = () => {
+	return (
+		<div>
+			
+		</div>
+	);
+}
+
+export default Home
